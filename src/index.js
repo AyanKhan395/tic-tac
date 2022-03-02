6d79a8059ba2f1a4cd3fad19759844707e2f961a
Ayan Ahmad Khan <ayyanahmad932@gmail.com>
2022-03-02 17:59:33 +0500

    Draw Match + Undo Redo + Status Fade + Cursor Disable

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+let cursorStyle;
+
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick} style={cursorStyle}>
       {props.value}
     </button>
   );
@@ -15,7 +18,7 @@ function Square(props) {
 class Board extends React.Component {
   renderSquare(i) {
     return (
-      <Square
+      <Square 
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -25,7 +28,7 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
+        <div className="board-row" >
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
@@ -84,11 +87,44 @@ class Game extends React.Component {
       xIsNext: (step % 2) === 0
     });
   }
+  undoFunction(step) {
+    if(step > 0 && this.state.stepNumber > 0){
+      if(step > 0 && step === this.state.stepNumber){
+        this.setState({
+          stepNumber: step-1,
+          xIsNext: ((step-1) % 2) === 0
+        });
+      }else if(step !== this.state.stepNumber){
+        this.setState({
+          stepNumber: this.state.stepNumber-1,
+          xIsNext: ((this.state.stepNumber-1) % 2) === 0
+        });
+      }
+    }
+  }
+  
+  
+  redoFunction(step) {
+    if(step < this.state.history.length && this.state.stepNumber < this.state.history.length){
+      if(step < 10 && step === this.state.stepNumber){
+        this.setState({
+          stepNumber: step,
+          xIsNext: ((step) % 2) === 0
+        });
+      }else if(step !== this.state.stepNumber){
+        this.setState({
+          stepNumber: this.state.stepNumber+1,
+          xIsNext: ((this.state.stepNumber+1) % 2) === 0
+        });
+      }
+    }
+  }
 
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+    const draw = calculateDraw(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ?
@@ -100,24 +136,57 @@ class Game extends React.Component {
         </li>
       );
     });
+    
+    let moveStep = history.length-1;
+    const movesFunction = (
+    <div>
+        <li >
+          <button onClick={() => this.undoFunction(moveStep)}>Undo</button>
+          
+          <button onClick={() => this.redoFunction(moveStep)}>Redo</button>
+        </li>
+    </div>
+  );
+    
 
     let status;
+    let divStyle;
     if (winner) {
       status = "Winner: " + winner;
+      divStyle = {
+        color: 'green',
+      };
+      cursorStyle = {
+        cursor: 'not-allowed',
+        color: 'grey',
+      };
+    } else if (draw) {
+      status = "Game Draw";
+      divStyle = {
+        color: 'grey',
+      };
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      divStyle = {
+        color: 'black',
+      };
+      cursorStyle = {
+        cursor: 'default',
+        color: 'black',
+      };
     }
 
     return (
       <div className="game">
-        <div className="game-board">
+        <div className="game-board" >
           <Board
             squares={current.squares}
             onClick={i => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
+          <div style={divStyle}>{status}</div>
+          <ul>{movesFunction}</ul>
           <ol>{moves}</ol>
         </div>
       </div>
@@ -148,6 +217,21 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+function calculateDraw(squares) {
+  let count = 0;
+  for (let i = 0; i < 9; i++) {
+    if (squares[i] !== null) {
+      count++;
+    }
+  }
+  if(count > 8){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
 
 
 // ========================================
